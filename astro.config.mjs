18dbce4c759b0fd5f059ea08933817a1e811debc
Kristian Lenovo <kristianhnielsen@protonmail.com>
2023-02-04 10:56:32 +0800

    update sitemap filter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.kristiannielsen.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap({
+      // Do not include the following paths to sitemap
+      filter: (page) => page !== "https://www.kristiannielsen.com/email/tak/",
+    }),
+  ],
 });
